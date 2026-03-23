9afabe579fbe66ddeadfb4f9a993dae5defa4929
TienQuan84 <phamtienquanhutl@gmail.com>
2026-03-23 15:44:15 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,5 @@
+const THU_TUC_DATA = {
+    "khai sinh": "Hồ sơ gồm: Giấy chứng sinh, CCCD cha mẹ. Thời gian: Trong ngày. Lệ phí: Miễn phí.",
+    "kết hôn": "Hồ sơ gồm: Tờ khai đăng ký, CCCD 2 bên, Giấy xác nhận tình trạng hôn nhân. Lưu ý: Cả 2 phải có mặt.",
+    "đất đai": "Vui lòng liên hệ bộ phận địa chính tại tầng 1 để được đo đạc và hướng dẫn nộp hồ sơ sổ đỏ."
+};
